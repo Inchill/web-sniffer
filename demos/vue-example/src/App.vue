237c90ab98fd408ccffac04f986ad708a5b4d0f8
liuyu <173772140@qq.com>
2022-11-28 18:30:35 +0800

    demos: vue use case

---
+++ demos/vue-example/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld data-expose="hello world expose" msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import WebSniffer from 'web-sniffer/dist/web-sniffer.esm'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  mounted () {
+    let ws = new WebSniffer({
+      url: '//localhost:8081'
+    })
+    ws.createDomWatcher({
+      root: document.documentElement
+    })
+
+    let img = document.createElement('img')
+    img.src = '//localhost:8081/101.png'
+    document.body.appendChild(img)
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
