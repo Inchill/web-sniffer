f4b0caa4b194cd4d5d637a0aa56ca7d533e4d318
liuyu <173772140@qq.com>
2022-11-29 10:18:50 +0800

    demos: data-event usage update

--- demos/react-example/src/components/hello.js
+++ demos/react-example/src/components/hello.js
@@ -1,5 +1,5 @@
 export default function Hello () {
   return (
-    <h1 data-expose="hello world" data-event-click="clicked me">hello world</h1>
+    <h1 data-expose="hello world" data-event="clicked me">hello world</h1>
   )
 }
